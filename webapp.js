bf0f4a9c917cabb16e7a6ced1af6a7f9e5442ef8
Tim Groß <44930867+Timhot3p@users.noreply.github.com>
2021-03-02 17:23:14 +0100

    zweites Game und neues Design

--- webapp.js
+++ webapp.js
@@ -1,5 +1,5 @@
-function changeColor() {
-    document.getElementById('button').style.color = 'red';
+function gege() {
+    document.getElementById('buttonGame').style.color = 'red';
     document.getElementById('ausgabe').textContent = "GG";
 }
 
@@ -12,7 +12,36 @@ function getRandomPosition(element) {
 }
 
 function changePosition() {
-    var xy = getRandomPosition(document.getElementById('button')); 
-    document.getElementById("button").style.left = xy[0] + 'px';
-    document.getElementById("button").style.top = xy[1] + 'px';    
+    var xy = getRandomPosition(document.getElementById('buttonGame')); 
+    document.getElementById('buttonGame').style.left = xy[0] + 'px';
+    document.getElementById('buttonGame').style.top = xy[1] + 'px';    
 }
+
+function getName() {
+    document.getElementById("ausgabe").textContent = window.location.pathname.split('/').pop;
+}
+(function() {
+     if (window.location.pathname.split('/').pop() == 'dontPress.html') {
+        document.onmousemove = handleMouseMove;
+        function handleMouseMove(event) {
+            var eventDoc, doc, body;
+
+            event = event || window.event; 
+            if (event.pageX == null && event.clientX != null) {
+                eventDoc = (event.target && event.target.ownerDocument) || document;
+                doc = eventDoc.documentElement;
+                body = eventDoc.body;
+
+                event.pageX = event.clientX +
+                  (doc && doc.scrollLeft || body && body.scrollLeft || 0) -
+                  (doc && doc.clientLeft || body && body.clientLeft || 0);
+                event.pageY = event.clientY +
+                  (doc && doc.scrollTop  || body && body.scrollTop  || 0) -
+                  (doc && doc.clientTop  || body && body.clientTop  || 0 );
+            }
+
+            document.getElementById('buttonGame').style.left = event.pageX + 'px';
+            document.getElementById('buttonGame').style.top = event.pageY + 'px'; 
+        }    
+     }
+})();
