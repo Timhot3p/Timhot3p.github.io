b8e2165e69547c9e30c18d2c18beb4338599acea
Tim Groß <44930867+Timhot3p@users.noreply.github.com>
2023-01-06 21:30:53 +0100

    Improved UI, set default values

--- dungeonpause.js
+++ dungeonpause.js
@@ -1,3 +1,4 @@
 function calc() {
-    document.getElementById("result").textContent = "Dungeonpause is good";
+    document.getElementById("result_dp").textContent = "It took you X days to reach Level 500.";
+    document.getElementById("result_normal").textContent = "Without Dungeonpause it would have taken you Y days.";
 }
