1de50794a4a33d62d1a29072279eea6ad9ac74b7
Tim Groß <44930867+Timhot3p@users.noreply.github.com>
2023-01-07 20:03:19 +0100

    improved Ui, added optimal time field, example log output

--- dungeonpause.js
+++ dungeonpause.js
@@ -1,4 +1,6 @@
 function calc() {
-    document.getElementById("result_dp").textContent = "It took you X days to reach Level 500.";
-    document.getElementById("result_normal").textContent = "Without Dungeonpause it would have taken you Y days.";
+    document.getElementById("result_dp").textContent = "It will take you X days to reach level " + document.getElementById("goal_lvl").value + ".";
+    document.getElementById("result_normal").textContent = "Without dungeonpause it would take you Y days.";
+    document.getElementById("result_opt").textContent = "Optimal time to reach level " + document.getElementById("goal_lvl").value + " would be Z days, by doing a dungeonpause between level A and level B.";
+    document.getElementById("output_log").textContent = "Day 1: Level 25 \n... \nDay X: Level " + document.getElementById("goal_lvl").value;
 }
