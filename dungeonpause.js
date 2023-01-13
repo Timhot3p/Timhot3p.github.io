b483daa104f836ec5ae523b7cc99ece22dc73d23
Tim Groß <44930867+Timhot3p@users.noreply.github.com>
2023-01-13 16:29:46 +0100

    first daily xp functions
    copied important functions from sftools, implemented basic daily xp functions and some temp output

--- dungeonpause.js
+++ dungeonpause.js
@@ -1,8 +1,17 @@
 function calc() {
+    let level = document.getElementById("current_lvl").value;
     document.getElementById("result_dp").textContent = "It will take you X days to reach level " + document.getElementById("goal_lvl").value + ".";
     document.getElementById("result_normal").textContent = "Without dungeonpause it would take you Y days.";
     document.getElementById("result_opt").textContent = "Optimal time to reach level " + document.getElementById("goal_lvl").value + " would be Z days, by doing a dungeonpause between level A and level B.";
-    document.getElementById("output_log").textContent = "Day 1: Level 25 \n... \nDay X: Level " + document.getElementById("goal_lvl").value;
+    //document.getElementById("output_log").textContent = "Day 1: Level 25 \n... \nDay X: Level " + document.getElementById("goal_lvl").value;
+    document.getElementById("output_log").textContent =
+        "Thirst: " + Math.round(getDailyXpThirst(level)) +
+        "\nDaily Mission (Hydra " + getHydraByLevel(level) + ") : " + Math.round(getDailyXpMission(level)) +
+        "\nAcademy: " + Math.round(getDailyXpAcademy(level)) +
+        "\nArena: " + Math.round(getDailyXpArena(level)) +
+        "\nWheel: " + Math.round(getDailyXpWheel(level)) + 
+        "\nTotal: " + Math.round(getDailyXp(level));
+    
 }
 
 // example player
@@ -26,18 +35,21 @@ function doLvlUp(player) {
 }
 
 //collects all daily xp and adds them together, checks for xp event
-function getDailyXp(player) {
-    return 0;
+function getDailyXp(level) {
+    return getDailyXpThirst(level) + getDailyXpAcademy(level) + + getDailyXpArena(level) + getDailyXpMission(level) + getDailyXpWheel(level);
 }
 
 //daily xp from thirst ignoring events
 function getDailyXpThirst(level) {
-    return 0;
+    let book = 96;
+    let questFactor = 0.85/2.5;
+    let thirst = 330;
+    return thirst * questFactor * getMaxXp(level, book);
 }
 
 //daily xp from arena ignoring events
 function getDailyXpArena(level) {
-    return 0;
+    return 10 * getArenaXp(level);
 }
 
 //daily xp from calendar considering the day
@@ -45,9 +57,14 @@ function getDailyXpCalendar(level, day) {
     return 0;
 }
 
+//Estimates what Hydra you can defeat with which level
+function getHydraByLevel(level) {
+    return Math.max(0, Math.floor((level - 300) / 15));
+}
+
 //daily xp of the daily secret mission ignoring events
 function getDailyXpMission(level) {
-    return 0;
+    return getHydraXp(level, getHydraByLevel(level));
 }
 
 //daily xp of the guild fight ignoring events
@@ -57,12 +74,13 @@ function getDailyXpGuildfight(level) {
 
 //daily xp of academy ignoring events
 function getDailyXpAcademy(level) {
-    return 0;
+    let hours = 24;
+    return hours * getAcademyValues(level, 20);
 }
 
 //daily xp of wheel ignoring events
 function getDailyXpWheel(level) {
-    return 0;
+    return 3 * getBigWheel(level);
 }
 
 //xp of dungeons possible considering a players level, writes to log
